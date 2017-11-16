1d2560ef2e87cccd52afd781fa1d9f0cdc974a41
Simon Gonzalez <ximonali@gmail.com>
2017-11-15 20:58:32 -0500

    Adding pages + Setting rootPage as TabsPage.

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ShoppingListPage } from '../shopping-list/shopping-list';
+import { RecipesPage } from '../recipes/recipes';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+  slPage = ShoppingListPage;
+  recipesPage = RecipesPage
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabsPage');
+  }
+
+}
